db5dfad9c24beeffca693403add3561e4e4a9696
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-03-14 14:55:32 -0400

    Document behaviour of cancelInput

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/workflow/Workflow.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/workflow/Workflow.java
@@ -11,6 +11,10 @@ public interface Workflow {
 
   void processInput(ProgressStep step);
 
+  /**
+   * Remove the last ProgressStep
+   * If no input has been processed so far, do nothing
+   */
   void cancelInput();
 
   void setProgress(Progress progress);
